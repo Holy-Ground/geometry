a3ecc124e16b2c3e6c9e597a5f2b10a07972a79f
movingChurch <seong9115@gmail.com>
2023-12-20 15:23:05 +0900

    Added basic description for point2d.hpp

---
+++ include/geometry/point2d.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file geometry/point2d.hpp
+ * @author EunSung Yang (seong9115@gmail.com)
+ * @brief Point class declaration with 2-dimension
+ * @version 1.0.0
+ * @date 2023-12-20
+ * @copyright Copyright (c) 2023 HolyGround, All Rights Reserved.
+ */
+
+// Copyright (c) 2023 HolyGround, All Rights Reserved.
+// Authors: EunSung Yang
+
+#ifndef HOLYGROUND__GEOMETRY__POINT_2_HPP_
+#define HOLYGROUND__GEOMETRY__POINT_2_HPP_
+
+#endif  // HOLYGROUND__GEOMETRY__POINT_2_HPP_
