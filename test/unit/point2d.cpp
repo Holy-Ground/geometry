c9b68df4a8a7cc764bc798d74c3a6f8f095143ab
movingChurch <seong9115@gmail.com>
2023-12-22 13:15:54 +0900

    Added test function to repo

---
+++ test/unit/point2d.cpp
@@ -0,0 +1,7 @@
+// Copyright (c) 2023 HolyGround, All Rights Reserved.
+// Authors: EunSung Yang
+
+#include <limits>
+#include <stdexcept>
+
+namespace holyground::geometry {}  // namespace holyground::geometry
